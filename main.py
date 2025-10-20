8ef2ae559e016cb663a0b144da9a4c565368a568
dineshtyagi1511 <dineshtyagi15112002@gmail.com>
2025-10-21 00:41:24 +0530

    patients

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+import json 
 
 app = FastAPI()
 
+def load_data():
+    with open("patients.json","r")as f :
+        data = json.load(f)
+
+    return data
+
+
+
 @app.get("/")
 def hello():
-    return {"message":"Hello World"}
+    return {"message":"Patient Management system api "}
 
 @app.get("/about")
 def about():
-    return {"ABOUT"}
+    return {"message":"a fully functional API "}
+
+@app.get("/view")
+def view():
+    data_ = load_data()
+    return data_

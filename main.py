d547a82523a599c12a0f37b9ec149b6534611312
dineshtyagi1511 <dineshtyagi15112002@gmail.com>
2025-10-21 16:41:20 +0530

    query and param

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Path ,HTTPException ,Query
 import json 
 
 app = FastAPI()
@@ -23,3 +23,32 @@ def about():
 def view():
     data_ = load_data()
     return data_
+
+@app.get("/view_patients/{patient_id}")
+def view(patient_id:str=Path(...,description="ID of the patient in the DB",example= "P001")):
+    data_= load_data()
+
+    if patient_id in data_:
+        return data_[patient_id]
+    
+    raise HTTPException(status_code=404,detail="patient not found")
+
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='sort in asc or desc order')):
+
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select between asc and desc')
+    
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
